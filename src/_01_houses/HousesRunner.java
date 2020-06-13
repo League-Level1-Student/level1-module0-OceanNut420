30810bf9e1d65197cbd1ab940a56f09413d0c05c
OceanNut420 <54760319+OceanNut420@users.noreply.github.com>
2020-06-12 18:26:41 -0700

    Update HousesRunner.java

--- src/_01_houses/HousesRunner.java
+++ src/_01_houses/HousesRunner.java
@@ -1,12 +1,14 @@
 package _01_houses;
 
+import org.jointheleague.graphical.robot.Robot;
+
 public class HousesRunner {
 
 	public static void main(String[] args) {
 		//Here we call the run() method from the Houses class
 		Houses houseBuilder = new Houses();
 		houseBuilder.run();
-
+Robot cat = new Robot();
 
 	}
 
