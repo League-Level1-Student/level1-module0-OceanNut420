88e04d60ff48c7fbcbbc6eac057c2eebccf4ff07
OceanNut420 <54760319+OceanNut420@users.noreply.github.com>
2020-06-19 18:27:53 -0700

    Update HousesRunner.java

--- src/_01_houses/HousesRunner.java
+++ src/_01_houses/HousesRunner.java
@@ -8,8 +8,37 @@ public class HousesRunner {
 		//Here we call the run() method from the Houses class
 		Houses houseBuilder = new Houses();
 		houseBuilder.run();
-Robot cat = new Robot();
+		Robot cat = new Robot();
+		cat.setSpeed(1000);
+		cat.move(-250);
+		cat.turn(90);
+		cat.move(-400);
+		cat.turn(-90);
+		cat.setPenWidth(10);
+		cat.penDown();
+	for(int i = 0; i < 10; i++) {	
+		house(cat);
+	}
+
+	}
+	static void house(Robot cat) {
+		cat.move(10);
+		cat.penDown();
+		cat.setSpeed(1000);
+		cat.move(90);
+		cat.turn(90);
+		cat.move(30);
+		cat.turn(90);
+		cat.move(100);
+		cat.turn(-90);
+		cat.setPenColor(0,255,0);
+		cat.move(20);
+		cat.turn(-90);
+		cat.setPenColor(0,0,0);
+		cat.penUp();
 
 	}
 
+	
+	
 }
