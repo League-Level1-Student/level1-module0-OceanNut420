46589c41af7d4d282727d29aea0186c61e84d688
OceanNut420 <54760319+OceanNut420@users.noreply.github.com>
2020-06-26 18:11:56 -0700

    Update HousesRunner.java

--- src/_01_houses/HousesRunner.java
+++ src/_01_houses/HousesRunner.java
@@ -1,5 +1,7 @@
 package _01_houses;
 
+import java.util.Random;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class HousesRunner {
@@ -9,33 +11,42 @@ public class HousesRunner {
 		Houses houseBuilder = new Houses();
 		houseBuilder.run();
 		Robot cat = new Robot();
-		cat.setSpeed(1000);
-		cat.move(-250);
-		cat.turn(90);
-		cat.move(-400);
-		cat.turn(-90);
+		cat.setSpeed(10000);
 		cat.setPenWidth(10);
-		cat.penDown();
-	for(int i = 0; i < 10; i++) {	
-		house(cat);
+		cat.turn(180);
+		cat.move(255);
+		cat.turn(90);
+		cat.move(400);
+		cat.turn(90);
+		
+		int hieght = 20;
+		
+			for(int i = 0; i < 10; i++) {	
+		
+		house(cat, hieght);
+		hieght = hieght + 10;
+	
 	}
 
 	}
-	static void house(Robot cat) {
+	static void house(Robot cat, int hieght) {
 		cat.move(10);
 		cat.penDown();
 		cat.setSpeed(1000);
-		cat.move(90);
+		cat.move(hieght - 10);
 		cat.turn(90);
 		cat.move(30);
 		cat.turn(90);
-		cat.move(100);
+		cat.move(hieght);
 		cat.turn(-90);
+		cat.move(11);
 		cat.setPenColor(0,255,0);
-		cat.move(20);
-		cat.turn(-90);
+		cat.move(13);
 		cat.setPenColor(0,0,0);
+		cat.move(2);
+		cat.turn(-90);
 		cat.penUp();
+		
 
 	}
 
