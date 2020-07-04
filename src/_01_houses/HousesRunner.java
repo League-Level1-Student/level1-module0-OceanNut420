a13ba8701973d2518dabe2a4b3b24e2460ea1c50
OceanNut420 <54760319+OceanNut420@users.noreply.github.com>
2020-07-03 18:29:02 -0700

    ererrere

--- src/_01_houses/HousesRunner.java
+++ src/_01_houses/HousesRunner.java
@@ -1,5 +1,6 @@
 package _01_houses;
 
+import java.awt.Color;
 import java.util.Random;
 
 import org.jointheleague.graphical.robot.Robot;
@@ -18,35 +19,80 @@ public class HousesRunner {
 		cat.turn(90);
 		cat.move(400);
 		cat.turn(90);
+		String hieght = "small";
+		Random ree = new Random();
 		
-		int hieght = 20;
-		
+		cat.setWindowColor(Color.black);
 			for(int i = 0; i < 10; i++) {	
+				int mee = ree.nextInt(3);
+				if(mee == 0) {
+			hieght = "small";
+		}
+		if(mee == 1) {
+			hieght = "medium";
+		}
+		if(mee == 2) {
+			hieght = "large";
+		}
+
+
+		house(cat, hieght, Color.WHITE);
 		
-		house(cat, hieght);
-		hieght = hieght + 10;
 	
 	}
 
 	}
-	static void house(Robot cat, int hieght) {
-		cat.move(10);
-		cat.penDown();
-		cat.setSpeed(1000);
-		cat.move(hieght - 10);
-		cat.turn(90);
-		cat.move(30);
-		cat.turn(90);
-		cat.move(hieght);
-		cat.turn(-90);
-		cat.move(11);
-		cat.setPenColor(0,255,0);
-		cat.move(13);
-		cat.setPenColor(0,0,0);
-		cat.move(2);
-		cat.turn(-90);
-		cat.penUp();
-		
+	static void house(Robot cat, String hieght, Color color) {
+		int bucket = 60;
+		if(hieght .equals("small")) {
+			bucket = 60;
+		}
+		if(hieght .equals("medium")) {
+			bucket = 120;
+		}
+		if(hieght .equals("large")) {
+			bucket = 250;
+			cat.setPenColor(color);
+			cat.move(10);
+			cat.penDown();
+			cat.setSpeed(1000);
+			cat.move(bucket - 10);
+			cat.turn(90);
+			cat.move(30);
+			cat.turn(90);
+			cat.move(bucket);
+			cat.turn(-90);
+			cat.move(11);
+			cat.setPenColor(0,255,0);
+			cat.move(13);
+			cat.setPenColor(color);
+			cat.move(2);
+			cat.turn(-90);
+			cat.penUp();
+
+		}
+		else {
+			cat.setPenColor(color);
+			cat.move(10);
+			cat.penDown();
+			cat.setSpeed(1000);
+			cat.move(bucket - 10);
+			cat.turn(45);
+			cat.move(25);
+			cat.turn(90);
+			cat.move(25);
+			cat.turn(45);
+			cat.move(bucket);
+			cat.turn(-90);
+			cat.move(11);
+			cat.setPenColor(0,255,0);
+			cat.move(13);
+			cat.setPenColor(color);
+			cat.move(2);
+			cat.turn(-90);
+			cat.penUp();
+
+				}
 
 	}
 
