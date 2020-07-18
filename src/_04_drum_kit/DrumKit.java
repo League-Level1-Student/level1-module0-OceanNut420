b523fc260aa4b015c9434af36fb4cef31e4cecbe
OceanNut420 <54760319+OceanNut420@users.noreply.github.com>
2020-07-17 18:31:09 -0700

    exaxa

--- src/_04_drum_kit/DrumKit.java
+++ src/_04_drum_kit/DrumKit.java
@@ -28,6 +28,7 @@ public class DrumKit implements MouseListener {
 		JFrame stink = new JFrame();
 		// 2. Make the frame visible and
 		// set its default close operation to JFrame.EXIT_ON_CLOSE
+		stink.setVisible(true);
 		stink.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		// 3. Set the size of the frame
 		stink.setSize(100,100);
@@ -39,21 +40,21 @@ public class DrumKit implements MouseListener {
 		stink.add(street);
 		// 7. Download an image of a drum from the Internet. Drop it into your
 		// Eclipse project under "default package".
-
+		
 		// 8. Put the name of your image file in a String variable.
-
+		String kitkat = "yookelaelae.jpg";
 		// 9. Edit the next line to use your String variable
-		// drumLabelWithImage = createLabelImage(drumImageString);
+		drumLabelWithImage = createLabelImage(kitkat);
 
 		// 10. Add the image to the panel
-
+		street.add(drumLabelWithImage);
 		// 11. Set the layout of the panel to "new GridLayout()"
-
+		street.setLayout(new GridLayout());
 		// 12. call the pack() method on the frame. Run your program. Do you see
 		// your drum image?
-
+		stink.pack();
 		// 13. add this mouse listener to drumLabelWithImage
-
+		addmouselistenertodrumLabelWithImage;
 		// 18. Add more images to make a drumkit. Remember to add this mouse
 		// listener to each one.
 
