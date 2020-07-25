3657480ee69538fa8c9373007474fca05e793f58
OceanNut420 <54760319+OceanNut420@users.noreply.github.com>
2020-07-24 18:29:56 -0700

    e

--- src/_04_drum_kit/DrumKit.java
+++ src/_04_drum_kit/DrumKit.java
@@ -21,7 +21,7 @@ import javax.swing.JPanel;
 public class DrumKit implements MouseListener {
 
 	JLabel drumLabelWithImage;
-
+	JLabel dogfat;
 	public void run() throws MalformedURLException {
 
 		// 1. Make a JFrame variable and initialize it using "new JFrame()"
@@ -45,25 +45,27 @@ public class DrumKit implements MouseListener {
 		String kitkat = "yookelaelae.jpg";
 		// 9. Edit the next line to use your String variable
 		drumLabelWithImage = createLabelImage(kitkat);
-
+		dogfat = createLabelImage("cymbal.jpg");
 		// 10. Add the image to the panel
 		street.add(drumLabelWithImage);
+		street.add(dogfat);
 		// 11. Set the layout of the panel to "new GridLayout()"
 		street.setLayout(new GridLayout());
 		// 12. call the pack() method on the frame. Run your program. Do you see
 		// your drum image?
 		stink.pack();
 		// 13. add this mouse listener to drumLabelWithImage
-		addmouselistenertodrumLabelWithImage;
+		drumLabelWithImage.addMouseListener(this);
+		dogfat.addMouseListener(this);
 		// 18. Add more images to make a drumkit. Remember to add this mouse
 		// listener to each one.
-
+		
 	}
 
 	public void mouseClicked(MouseEvent e) {
 		// 14. Print "mouse clicked" to the console. Run your program and watch
 		// the console to see when this is printed.
-
+		System.out.println("mouse clicked");
 		JLabel drumClicked = (JLabel) e.getSource(); // This line gets the label
 														// that the mouse
 														// clicked on
@@ -71,12 +73,17 @@ public class DrumKit implements MouseListener {
 		// 15. Download a drum sound and drop it into your "default package".
 		// You can find it on freesound.org. To download it, log in as
 		// leagueofamazing/code4life.
-
+		
 		// 16. If they clicked on the drumImage...
-
+		if(drumClicked == drumLabelWithImage) {
 		// 17. ...use the playSound method to play a drum sound. Test to see if
 		// it works
-
+			playSound("drum.wav");
+			
+		}
+		if(drumClicked == dogfat) {
+			playSound("src/_04_drum_kit/cymbal.wav");
+		}
 	}
 
 	private JLabel createLabelImage(String fileName) throws MalformedURLException {
