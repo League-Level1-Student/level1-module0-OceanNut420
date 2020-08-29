5b8153af5854693a87bf2d65763c8323a4dc5333
OceanNut420 <54760319+OceanNut420@users.noreply.github.com>
2020-08-28 18:30:37 -0700

    beedrill

--- src/_09_sound_effects_machine/Dogphat.java
+++ src/_09_sound_effects_machine/Dogphat.java
@@ -1,20 +1,29 @@
 package _09_sound_effects_machine;
 
 import javax.swing.JApplet;
+import javax.swing.JButton;
+import javax.swing.JFrame;
 
 public class Dogphat {
-
+	
 	public static void main(String[] args) {
-		
 
+		JFrame frame = new JFrame();
+        new Dogphat().showButton();
+        frame.setVisible(true);
+        JButton button = new JButton();
+        frame.add(button);
+        frame.pack();
+        button.addActionListener(this);
+        
 	}
 	public void showButton() {
         System.out.println("Button clicked");
    }
-	public class FortuneCookieRunner {
+
+
+    
       
-            new FortuneCookie().showButton();
-      }
    
 	private void playSound(String fileName) {
 	      AudioClip sound = JApplet.newAudioClip(getClass().getResource(fileName)); 
